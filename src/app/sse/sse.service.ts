e94be3272c3f594eadb66ba6163aa4da0f67314d
Bjorn De Craemer <bjorn.decraemer@yahoo.com>
2020-02-28 00:14:47 +0100

    Fixed : Event types have to match !!!

--- src/app/sse/sse.service.ts
+++ src/app/sse/sse.service.ts
@@ -1,34 +1,31 @@
-import {Injectable, NgZone} from '@angular/core';
-import {Observable, ReplaySubject} from "rxjs";
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SseService {
 
-  public $tasks = new ReplaySubject();
+  constructor() { }
 
-  constructor(private _zone: NgZone) { }
-
-  getServerSentEvent(url: string): Observable<any>{
-    return Observable.create(observer => {
-      const eventSource = this.getEventSource(url);
-
+  newObservable<R>(path: string): Observable<R> {
+    return new Observable(observer => {
+      const eventSource = new EventSource(path);
       eventSource.onmessage = event => {
-        this._zone.run(() => {
-         observer.next(event);
-        });
+        observer.next(event.data);
       };
-
-      eventSource.onerror = error => {
-        this._zone.run(() => {
-          observer.error(error);
-        })
-      }
-    })
+      eventSource.onerror = () => {
+        if (eventSource.readyState !== eventSource.CONNECTING) {
+          observer.error('An error occurred.');
+          console.log("HERE2");
+        }
+        eventSource.close();
+        observer.complete();
+      };
+      return () => {
+        eventSource.close();
+      };
+    });
   }
 
-  private getEventSource(url: string): EventSource {
-    return new EventSource(url);
-  }
 }
--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -8,12 +8,27 @@ import {SseService} from "../../sse/sse.service";
 })
 export class TodoComponent implements OnInit {
 
+  private eventTypes = ['message','open','join','leave'];
+
   constructor(private sseService : SseService){}
 
   ngOnInit() {
+    console.log("watcherEvent");
+    // OLD CODESTYLE
+    // if (typeof(EventSource) !== 'undefined') {
+    //   const eventSource = new EventSource('http://localhost:8080/sse');
+    //   this.eventTypes.forEach((type: string) =>
+    //     eventSource.addEventListener(type, (message: any) => console.log(message))
+    //   );
+    //   //eventSource.onmessage = (message) => alert(message);
+    //   eventSource.onerror = (error) => console.log(error);
+    //   return () => {
+    //     eventSource.close();
+    //   };
+    // }
     this.sseService
-      .getServerSentEvent("http://localhost:8080/sse")
-      .subscribe(data => console.log(data));
+      .newObservable("http://localhost:8080/sse")
+      .subscribe((watcherEvent) => console.log(watcherEvent), error => console.log(error), () => console.log("COMPLETE"));
   }
 
 }
